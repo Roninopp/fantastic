6502f2b4ff85d70f3ea566d441ea5c6c60d225be
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-29 15:25:19 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -2,14 +2,16 @@ PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm A
 
 MIKU_IMG = (
       "https://telegra.ph/file/7f4a6de580ab6c1a6e57e.jpg",
-      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg"
+      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg",
+      "https://te.legra.ph/file/fb8584c4d654cc4aa4f24.jpg"
 )
 
 TEXXT = "<b>Heya</b> {} <b>I'm Samurai</b>"
 
 MIKU_N_IMG = (
       "https://telegra.ph/file/7f4a6de580ab6c1a6e57e.jpg",
-      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg*
+      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg",
+      "https://te.legra.ph/file/fb8584c4d654cc4aa4f24.jpg"
 
 )
 
