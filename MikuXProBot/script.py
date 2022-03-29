192340749836e3fc8ef53b32519dce1ee0ee0aa0
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-29 15:23:57 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -1,24 +1,18 @@
 PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @MikuXProBot For Repo"""
 
 MIKU_IMG = (
-      "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
-      "https://telegra.ph//file/05311caed13176f7d35e5.jpg",
-      "https://telegra.ph//file/88976abda0d0af9d4a517.jpg",
-      "https://telegra.ph//file/b388f473ddfb9cc727bb1.jpg",
-      "https://telegra.ph//file/d950844dad2e473950470.jpg",
+      "https://telegra.ph/file/7f4a6de580ab6c1a6e57e.jpg",
+      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg"
 )
 
-TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
+TEXXT = "<b>Heya</b> {} <b>I'm Samurai</b>"
 
 MIKU_N_IMG = (
-      "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
-      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
-      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
-      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
-      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+      "https://telegra.ph/file/7f4a6de580ab6c1a6e57e.jpg",
+      "https://telegra.ph/file/827fb45192c16e07c9af1.jpg*
 
 )
 
-PM_PHOTO = "https://telegra.ph//file/3a88a958a88e83d85d284.jpg"
+PM_PHOTO = "https://telegra.ph//file/9012a958e07362727ae19.jpg"
 
-MIKU_DISPACHER_PIC = "https://telegra.ph//file/4ce99db6136c77248e0ec.jpg"
+MIKU_DISPACHER_PIC = "https://telegra.ph//file/9012a958e07362727ae19.jpg"
