c46b715f0006036af9a2b5759ffbce36bc4b4181
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-29 23:38:24 +0530

    Update misc.py

--- MikuXProBot/modules/helper_funcs/misc.py
+++ MikuXProBot/modules/helper_funcs/misc.py
@@ -60,9 +60,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
     else:
         pairs += [[
-            (EqInlineKeyboardButton("Try inline", switch_inline_query_current_chat="",)),
+            (EqInlineKeyboardButton("OT", url="t.me/RONIN_Fighters_Fd",)),
                 EqInlineKeyboardButton("Back", callback_data="miku_back"),
-             EqInlineKeyboardButton("Support", url="t.me/Mikussupport")]]
+             EqInlineKeyboardButton("Support", url="t.me/liu_WULANG_MONKES")
 
     return pairs
 
